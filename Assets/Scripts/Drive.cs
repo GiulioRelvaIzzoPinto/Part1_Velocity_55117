9ce34064a2ca58bc35da5eec37b0b9b2f79a1ecc
Giulio <giuliorelvaip@gmail.com>
2024-11-06 00:42:26 +0000

    Calculating Trajectories Tutorial
    Final commit of "The Physics of AI" Tutorial of Part 1-1 of the AI tutorials.

--- Assets/Scripts/Drive.cs
+++ Assets/Scripts/Drive.cs
@@ -12,24 +12,36 @@ public class Drive : MonoBehaviour
 
     void Update()
     {
-        float translation = Input.GetAxis("Vertical") * rotationSpeed;
-        float rotation = Input.GetAxis("Horizontal") * speed;
+        // Get the horizontal and vertical axis.
+        // By default they are mapped to the arrow keys.
+        // The value is in the range -1 to 1
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
 
+        // Make it move 10 meters per second instead of 10 meters per frame...
         translation *= Time.deltaTime;
         rotation *= Time.deltaTime;
 
+        // Move translation along the object's z-axis
+        transform.Translate(0, 0, translation);
+        // transform.Translate(0, 0, speed * Time.deltaTime);
+
+        // Rotate around our y-axis
         transform.Rotate(0, rotation, 0);
 
         if (Input.GetKey(KeyCode.T))
         {
-            transGun.RotateAround(transGun.position, transGun.right, -2f);
+
+            transGun.RotateAround(transGun.position, transGun.right, -2.0f);
         }
         else if (Input.GetKey(KeyCode.G))
         {
-            transGun.RotateAround(transGun.position, transGun.right, 2f);
+
+            transGun.RotateAround(transGun.position, transGun.right, 2.0f);
         }
         else if (Input.GetKeyDown(KeyCode.B))
         {
+
             Instantiate(bulletObj, gun.position, gun.rotation);
         }
     }
